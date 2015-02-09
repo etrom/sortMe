b9424a0040fa752e59f3a04e42ead66d0c49b216
Elaine Trombley <elaine.trombley3@gmail.com>
2015-02-09 16:34:34 -0500

    cards have correct icons

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -35,75 +35,76 @@ angular.module('sortmeApp')
     $scope.classone = "top";
     $scope.classtwo = "middle";
     $scope.classthree = "bottom";
-    $scope.click = false;
-    $scope.clicked= function(){
-        $scope.click = true;
-    }
 
-    $scope.changeClass = function($event){
-        debugger;
+
+    $scope.changeClass = function($event) {
+        var sib;
         if ($event.target.classList[1]  === 'top' || $event.target.classList[0]  === 'top') {
+
         }
         //sitches middle
-         if ($event.target.classList[1]=== 'middle') {
-            var sib = $event.target.parentElement.parentElement.children[0].children[0].className;
+        if ($event.target.classList[1] === 'middle') {
+
+            sib = $event.target.parentElement.parentElement.children[0].children[0].className;
             if(sib === 'middle') {
                 $event.target.parentElement.parentElement.children[1].children[0].className ='top';
                 $event.target.className = 'middle';
+            } else if(sib === 'bottom') {
+                $event.target.parentElement.parentElement.children[2].children[0].className = 'middle';
+                $event.target.className = 'top';
             } else {
-                    $event.target.parentElement.parentElement.children[0].children[0].className = 'middle';
-                    $event.target.className = "top"
+                $event.target.parentElement.parentElement.children[0].children[0].className = 'middle';
+                $event.target.className = "top"
             }
-
         }
         //switches middle
         if ($event.target.classList[0]=== 'middle') {
-            var sib = $event.target.parentElement.parentElement.children[1].children[0].className;
-                            console.log(sib,'sibling')
+             sib = $event.target.parentElement.parentElement.children[1].children[0].className;
             if (sib === 'middle') {
                $event.target.parentElement.parentElement.children[0].children[0].className ='middle';
                 $event.target.className = 'top'
-                } else {
-                    $event.target.parentElement.parentElement.children[1].children[0].className = 'middle';
-                    $event.target.className = "top"
-                }
-
+            } else if(sib === 'bottom') {
+                $event.target.parentElement.parentElement.children[2].children[0].className = 'middle';
+                $event.target.className = 'top';
+            } else {
+                $event.target.parentElement.parentElement.children[1].children[0].className = 'middle';
+                $event.target.className = "top"
+            }
         }
         //switches bottom
         if ($event.target.classList[1]=== 'bottom') {
-            var sib = $event.target.parentElement.parentElement.children[0].children[0].className;
+             sib = $event.target.parentElement.parentElement.children[0].children[0].className;
             if(sib === 'bottom') {
                 $event.target.parentElement.parentElement.children[2].children[0].className ='top';
                 $event.target.className = 'bottom';
+            } else if(sib === 'middle') {
+                $event.target.parentElement.parentElement.children[1].children[0].className = 'bottom'
+                $event.target.className = 'top';
             } else {
-                    $event.target.parentElement.parentElement.children[0].children[0].className = 'bottom';
-                    $event.target.className = "top"
+                $event.target.parentElement.parentElement.children[0].children[0].className = 'bottom';
+                $event.target.className = "top"
+            }
+        }
+        //switches bottom
+        if ($event.target.classList[0] === 'bottom') {
+            debugger;
+             sib = $event.target.parentElement.parentElement.children[1].children[0].className;
+            if (sib === 'bottom' && $event.target.className !== sib) {
+                $event.target.parentElement.parentElement.children[1].children[0].className ='bottom';
+                // $event.target.parentElement.parentElement.children[0].children[0].className ='middle';
+                $event.target.className = 'top'
+
+            } else if(sib === 'middle' && $event.target.className !== $event.target.parentElement.parentElement.children[0].children[0].className) {
+                $event.target.parentElement.parentElement.children[0].children[0].className = 'bottom'
+                $event.target.className = 'top';
+            } else {
+                $event.target.parentElement.parentElement.children[2].children[0].className = 'bottom';
+                $event.target.className = "top"
             }
 
-        }
-        if ($event.target.classList[0]=== 'bottom') {
-            var sib = $event.target.parentElement.parentElement.children[2].children[0].className;
-                            console.log(sib,'sibling')
-            if (sib === 'bottom') {
-               $event.target.parentElement.parentElement.children[0].children[0].className ='bottom';
-                $event.target.className = 'top'
-                } else {
-                    $event.target.parentElement.parentElement.children[2].children[0].className = 'bottom';
-                    $event.target.className = "top"
-                }
+
 
         }
-
-
-        //  if ($event.target.classList[1] === 'bottom'){
-
-
-
-        //  } || $event.target.classList[0]=== 'bottom') {
-        //    $event.target.parentElement.parentElement.children[0].children[0].className = 'bottom';
-        //     $event.target.className = "top"
-        // }
-
     };
 
 
