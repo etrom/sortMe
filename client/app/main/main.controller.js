b0c01e9f66862bc260315bda539f3d50a1b72535
Elaine Trombley <elaine.trombley3@gmail.com>
2015-02-08 20:23:51 -0500

    directive template

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,21 +1,68 @@
 'use strict';
 
 angular.module('sortmeApp')
+  .directive('ngCards', function() {
+      return {
+        restrict: 'A',
+        templateUrl: "'../templates/cards.html'"
+      }
+    })
   .controller('MainCtrl', function ($scope, $http) {
     $scope.things = [];
 
+    //gets the JSON data
     $http.get('/api/things').success(function(data) {
       $scope.things = data.data;
     });
 
-    Array.prototype.cut = function(cutSize) {
-    var array=this;
-    return [].concat.apply([],
-        array.map(function(elem,i) {
-            return i % cutSize ? [] : [array.slice(i,i+cutSize)];
-        })
-    );
-}
+    $scope.openModal = function() {
+
+    }
+
+    $scope.classone = "top";
+    $scope.classtwo="middle";
+    $scope.classthree="bottom";
+
+   //  $scope.changeClass = function(stuff, $event){
+   // //      if ($scope.class === "top") {
+   //           //             $scope.class = "bottom";
+   //               //     } else if ($scope.class === "bottom"){
+   //                //         $scope.class = "top";
+   //                 //   }
+   // console.log($event.target);
+   //      if (stuff === 'one'){
+   //          console.log("hey");
+   //          var temp = $scope.classtwo
+   //          $scope.classtwo = $scope.classone;
+   //          // middle is now top
+   //          $scope.classone = temp;
+   //          // top is now middle
+   //   }
+   //          //else if ($scope.classonetwo === "top"){
+   //    //       $scope.classonetwo = "middle";
+   //      //    $scope.classone = "top";
+   //      //}
+   //      if (stuff === 'two') {
+   //          var temp = $scope.classthree;
+   //          $scope.classthree = $scope.classone;
+   //          $scope.classone = temp;
+   //      }
+   //          //else if ($scope.classthree === "top"){
+   //       //    $scope.classthree = "bottom";
+   //         //  $scope.classone="top";
+   //    //  }
+   //  };
+
+
+    //cut an array and return
+    // Array.prototype.cut = function(cutSize) {
+    //     var array = this;
+    //     return [].concat.apply([],
+    //         array.map(function(elem,i) {
+    //             return i % cutSize ? [] : [array.slice(i ,i+cutSize)];
+    //         })
+    //     );
+    // }
 
 
   });
