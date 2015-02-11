b461cf789adbe2c2f8f5a627890526b10fd82f44
Elaine Trombley <elaine.trombley3@gmail.com>
2015-02-10 20:04:42 -0500

    modified app.js

--- client/app/app.js
+++ client/app/app.js
@@ -7,10 +7,11 @@ angular.module('sortmeApp', [
   'ui.router',
   'ui.bootstrap',
 
-])
+]).use(favicon(__dirname + '/client/favicon.ico'))
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
+
 
     $locationProvider.html5Mode(true);
   });
